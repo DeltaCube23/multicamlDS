d8b548afe0762f58cf90fc64c4beff014e6a7602
DeltaCube23 <adithyarajesh@gmail.com>
2023-05-31 17:46:50 +0200

    organize executables for benchmarking

--- bin/bq.ml
+++ bin/bq.ml
@@ -9,70 +9,24 @@ let worker q num_ops id =
   if id = 1 then begin
     for i = 1 to num_ops do
       let action = Random.int 2 in
-      if action = 0 then Ms_queue.push q i
-      else ignore (Ms_queue.pop q)
+      if action = 0 then Bounded_queue.push q i
+      else ignore (Bounded_queue.pop q)
     done
   end
   else if id = 2 then begin
     for i = 1 to num_ops do
-      Ms_queue.push q i
+      Bounded_queue.push q i
     done
   end
   else begin
     for _ = 1 to num_ops do
-      ignore (Ms_queue.pop q)
+      ignore (Bounded_queue.pop q)
     done
   end
 in
 
-(* 2 workers random push/pop *)
-let q = Ms_queue.init () in
-let start_time = Unix.gettimeofday () in
-let d1 = Domain.spawn (fun _ -> worker q task 1) in
-let d2 = Domain.spawn (fun _ -> worker q task 1) in
-Domain.join d1;
-Domain.join d2;
-let end_time = Unix.gettimeofday () in
-Format.printf "Completed 2 workers in %f\n" (end_time -. start_time);
-
-(* 4 workers random push/pop *)
-let q = Ms_queue.init () in
-let start_time = Unix.gettimeofday () in
-let d1 = Domain.spawn (fun _ -> worker q task 1) in
-let d2 = Domain.spawn (fun _ -> worker q task 1) in
-let d3 = Domain.spawn (fun _ -> worker q task 1) in
-let d4 = Domain.spawn (fun _ -> worker q task 1) in
-Domain.join d1;
-Domain.join d2;
-Domain.join d3;
-Domain.join d4;
-let end_time = Unix.gettimeofday () in
-Format.printf "Completed 4 workers in %f\n" (end_time -. start_time);
-
-(* 8 workers random push/pop *)
-let q = Ms_queue.init () in
-let start_time = Unix.gettimeofday () in
-let d1 = Domain.spawn (fun _ -> worker q task 1) in
-let d2 = Domain.spawn (fun _ -> worker q task 1) in
-let d3 = Domain.spawn (fun _ -> worker q task 1) in
-let d4 = Domain.spawn (fun _ -> worker q task 1) in
-let d5 = Domain.spawn (fun _ -> worker q task 1) in
-let d6 = Domain.spawn (fun _ -> worker q task 1) in
-let d7 = Domain.spawn (fun _ -> worker q task 1) in
-let d8 = Domain.spawn (fun _ -> worker q task 1) in
-Domain.join d1;
-Domain.join d2;
-Domain.join d3;
-Domain.join d4;
-Domain.join d5;
-Domain.join d6;
-Domain.join d7;
-Domain.join d8;
-let end_time = Unix.gettimeofday () in
-Format.printf "Completed 8 workers in %f\n" (end_time -. start_time);
-
 (* 2 producers 2 consumers *)
-let q = Ms_queue.init () in
+let q = Bounded_queue.init () in
 let start_time = Unix.gettimeofday () in
 let d1 = Domain.spawn (fun _ -> worker q task 2) in
 let d2 = Domain.spawn (fun _ -> worker q task 2) in
@@ -86,7 +40,7 @@ let end_time = Unix.gettimeofday () in
 Format.printf "Completed 2 producers 2 consumers in %f\n" (end_time -. start_time);
 
 (* 4 producers 4 consumers *)
-let q = Ms_queue.init () in
+let q = Bounded_queue.init () in
 let start_time = Unix.gettimeofday () in
 let d1 = Domain.spawn (fun _ -> worker q task 2) in
 let d2 = Domain.spawn (fun _ -> worker q task 2) in
@@ -108,7 +62,7 @@ let end_time = Unix.gettimeofday () in
 Format.printf "Completed 4 producers 4 consumers in %f\n" (end_time -. start_time);
 
 (* 1 producer 4 consumers *)
-let q = Ms_queue.init () in
+let q = Bounded_queue.init () in
 let start_time = Unix.gettimeofday () in
 let d1 = Domain.spawn (fun _ -> worker q (4*task) 2) in
 let d2 = Domain.spawn (fun _ -> worker q task 3) in
@@ -124,7 +78,7 @@ let end_time = Unix.gettimeofday () in
 Format.printf "Completed 1 producer 4 consumers in %f\n" (end_time -. start_time);
 
 (* 4 producers 1 consumer *)
-let q = Ms_queue.init () in
+let q = Bounded_queue.init () in
 let start_time = Unix.gettimeofday () in
 let d1 = Domain.spawn (fun _ -> worker q task 2) in
 let d2 = Domain.spawn (fun _ -> worker q task 2) in
